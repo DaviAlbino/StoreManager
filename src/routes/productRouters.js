2adb2d43c83b29a94847ef6bd10bd367c086ad65
Davi Albino <negroalbino_@hotmail.com>
2022-11-03 20:18:13 -0300

    req 4

--- src/routes/productRouters.js
+++ src/routes/productRouters.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const controller = require('../controllers/products.controller');
+const validateName = require('../middlewares/products.middleware');
 
 const router = express.Router();
 
 router.get('/', controller.getAll);
 router.get('/:id', controller.findById);
-router.post('/', controller.addNewProduct);
+router.post('/', validateName, controller.addNewProduct);
 // console.log(router.post('/', controller.addNewProduct));
 
 module.exports = router;
